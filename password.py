4d1363f3af6d8a2417cf2b6a7084d1e08ccbe32a
danasf9-dotcom <danasf9@gmail.com>
2026-08-25 19:56:30 +0000

    world cup

---
+++ password.py
@@ -0,0 +1,13 @@
+def main() :
+
+password = 2828
+user = int(input)("what is the password"))
+if user == password:
+       print("Correct password.")
+
+print("See you later.")
+
+
+if __name__ == "__main__":
+   main()
+
