24f62fee09c8794b88fd147fec465382a449de10
danasf9-dotcom <danasf9@gmail.com>
2026-08-27 15:44:36 +0000

    atmosphere

---
+++ atmosphere.py
@@ -0,0 +1,31 @@
+def main():
+   layer = input("Descent Atmosphere layer"))
+
+   if layer == Exsophere
+      print("Your altitude level will between 700–10,000 km")
+   elif layer == Thermosphere
+        print("Your altitude level will between 85–700 km")
+   elif layer == Mesosphere
+        print("Your altitude level will between 50–85 km")
+   elif layer == Stratosphere
+        print("Your altitude level will between 12–50 km")
+   elif layer == Troposphere
+        print("Your altitude level will between 0–12 km")
+   else :
+       print("Insert a valid atmosphere layer:")
+   exact = input("Enter exact altitude")
+   ex = 2000
+   me= 200
+   th = 500
+   st = 75
+   tr = 20
+   if exact > 699:
+       
+
+
+
+
+
+
+if __name__ == "__main__":
+   main()
