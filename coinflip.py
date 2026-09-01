aea7047a6fd501f53c21fe206ba01ca54fcf3218
danasf9-dotcom <danasf9@gmail.com>
2026-09-01 21:44:42 +0000

    coinflip

---
+++ coinflip.py
@@ -0,0 +1,20 @@
+import random
+def main():
+
+results = input("Heads or Tails")
+coin = random.radiant(1,2)
+
+if coin == 1:
+    results = "Heads"
+else:
+    results == "Tails"
+
+print("Guess the coin flip:", results)
+
+if guess == results
+print(you win!)
+else
+
+
+if __name__ == "__main__":
+   main()
