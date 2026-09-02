7f4d97204360840072ba0ee4ac786d1f89277215
danasf9-dotcom <danasf9@gmail.com>
2026-09-02 18:06:02 +0000

    roadtrip

---
+++ roadtrip.py
@@ -0,0 +1,18 @@
+def main():
+    # Finite loop
+    answer = ""
+    followup=""
+
+    while answer != "Yes!":
+        answer = input("Are we are there yet? ").title().strip()
+        if answer == "Yes":
+            followup = input("Really? ").title().strip()
+        if followup == "Yes":
+            break
+
+
+    print("We are here!")
+
+if __name__ == "__main__":
+    main()
+
