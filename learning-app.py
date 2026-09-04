0887610b331dd01fd9146d0204418364b4b66c35
danasf9-dotcom <danasf9@gmail.com>
2026-09-04 15:48:39 +0000

    terminar

---
+++ learning-app.py
@@ -0,0 +1,13 @@
+import random
+def main():
+    #App name
+    print("Welcome to my learning app")
+    n1 = random.randint(10, 99)
+    n2 = random.randint(10, 99)
+
+    print(f"Try to do this:{n1} + {n2}")
+    
+
+
+if __name__ == "__main__":
+    main()
