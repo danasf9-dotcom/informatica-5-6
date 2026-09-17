56c75c871a351d48369775cb2d68d251413b130f
danasf9-dotcom <danasf9@gmail.com>
2026-09-17 15:00:15 +0000

    to do list

---
+++ to-do.py
@@ -0,0 +1,16 @@
+def main():
+    tasks = [] # Empty list
+
+    while True:
+        print(f"Tasks to do: {len(tasks)}")
+        print(tasks)
+        command = input("What do you want to do? (add, complete, exit): ")
+        if command == "add":
+           new_task = input("Enter new task: ")
+           tasks.append(new_task)
+
+
+
+
+if __name__ == "__main__":
+   main()
