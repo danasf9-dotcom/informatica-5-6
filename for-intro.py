0597eb33fee5a05360266c822512f0cbe5f4c5f9
danasf9-dotcom <danasf9@gmail.com>
2026-09-18 20:20:33 +0000

    for intro

---
+++ for-intro.py
@@ -0,0 +1,15 @@
+def main():
+    doctrine = ["Faith in the lord jesus christ", "Repentance", "Baptism", "Imposicion de manos", "Perseverar hasta el fin"]
+
+    index = 0
+
+    # while index < len(doctrine): condition
+    #   print(doctrine[index])
+    #   index += 1 update
+
+    for principles in doctrine:
+        print(principles)
+
+if __name__ == "__main__":
+   main()
+
